a519d1ca5becc2dcb1cf501e09155b3f1c2ebb7e
정재하 <jjh4039@naver.com>
2025-09-15 14:48:29 +0900

    09.15 4주차 게임제작

--- Assets/Script/Man_Move.cs
+++ Assets/Script/Man_Move.cs
@@ -5,27 +5,28 @@ using UnityEngine.UI;
 
 public class Man_Move : MonoBehaviour
 {
- 
+    public Rigidbody2D rigid;
+
     void Update()
-    { 
-        if (Input.GetKey(KeyCode.RightArrow))
+    {
+        if (Input.GetKey(KeyCode.W))
         {
-            transform.Translate(0.01f, 0, 0);
+            rigid.AddForce(new Vector2(0, 5f));
         }
 
-        if(Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.A))
         {
-            transform.Translate(-0.01f, 0, 0);
+            transform.Translate(-10 * Time.deltaTime, 0, 0);
         }
 
-        if(Input.GetKey(KeyCode.UpArrow))
+        if (Input.GetKey(KeyCode.S))
         {
-            transform.Translate(0, 0.01f, 0);
+            rigid.AddForce(new Vector2(0, -5f));
         }
 
-        if(Input.GetKey(KeyCode.DownArrow))
+        if (Input.GetKey(KeyCode.D))
         {
-            transform.Translate(0, -0.01f, 0);
+            transform.Translate(10 * Time.deltaTime, 0, 0);
         }
     }
 }
