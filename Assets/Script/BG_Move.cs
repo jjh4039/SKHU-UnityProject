f0e8be255e7ea4608b46179e55d99791b514eb6a
정재하 <jjh4039@naver.com>
2025-09-22 14:43:21 +0900

    09.22 5주차 게임제작

---
+++ Assets/Script/BG_Move.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BG_Move : MonoBehaviour
+{
+    public Transform playerPos;
+    public SpriteRenderer spriteBG;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.position = new Vector3(playerPos.position.x, playerPos.position.y + 3f, this.transform.position.z);
+        spriteBG.material.mainTextureOffset = new Vector2(playerPos.position.x, playerPos.position.y) * 0.01f;
+    }
+}
---
+++ Assets/Script/Camera_Move.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Move : MonoBehaviour
+{
+    public Transform player;
+
+    void Update()
+    {
+        transform.position = new Vector3(player.position.x, player.position.y, this.transform.position.z);
+    }
+}
