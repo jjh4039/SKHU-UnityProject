057867ca49557fd37fcc4aa2f5ff5ecb0139b625
정재하 <jjh4039@naver.com>
2025-10-13 02:52:04 +0900

    Pixel

--- Assets/Script/BG_Move.cs
+++ Assets/Script/BG_Move.cs
@@ -14,7 +14,7 @@ public class BG_Move : MonoBehaviour
 
     void Update()
     {
-        transform.position = new Vector3(playerPos.position.x, playerPos.position.y + 3f, this.transform.position.z);
+        transform.position = new Vector3(playerPos.position.x, playerPos.position.y + 1f, this.transform.position.z);
         spriteBG.material.mainTextureOffset = new Vector2(playerPos.position.x, playerPos.position.y) * 0.01f;
     }
 }
